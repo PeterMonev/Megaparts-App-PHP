f7a997ccb922f5ad58fa0ca96b1fad257cb3be89
PeterMonev <monevpeter@gmail.com>
2023-09-04 12:10:46 +0300

    Creating section info and animations

--- client/animation.js
+++ client/animation.js
@@ -1,26 +1,26 @@
 //Sticky Navigation Bar 
-const lowerNav = document.querySelector("#lower__nav");
+// const lowerNav = document.querySelector("#lower__nav");
 
-function setSticky() {
-    gsap.set(lowerNav, {
-        position: "fixed",
-        top: 0,
-        left: 0,
-        right: 0
-    });
-}
+// function setSticky() {
+//     gsap.set(lowerNav, {
+//         position: "fixed",
+//         top: 0,
+//         left: 0,
+//         right: 0
+//     });
+// }
 
-function unsetSticky() {
-    gsap.set(lowerNav, { clearProps: "all" });
-}
+// function unsetSticky() {
+//     gsap.set(lowerNav, { clearProps: "all" });
+// }
 
-ScrollTrigger.create({
-    trigger: lowerNav,
-    start: "top top",
-    end: "bottom bottom",
-    onEnter: setSticky,
-    onLeaveBack: unsetSticky
-});
+// ScrollTrigger.create({
+//     trigger: lowerNav,
+//     start: "top top",
+//     end: "bottom bottom",
+//     onEnter: setSticky,
+//     onLeaveBack: unsetSticky
+// });
 
 // Carousel Banner Images
 $("#carouselExampleIndicators").swipe({
