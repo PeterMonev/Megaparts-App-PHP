10e67abf12f51efeca596200477badea44bc542d
PeterMonev <monevpeter@gmail.com>
2023-09-05 17:54:25 +0300

    Creating special offers section animations

--- client/animation.js
+++ client/animation.js
@@ -44,6 +44,31 @@ $('#div__specialOffers__offers').flickity({
     wrapAround: false,
   });
 
-  
+// Special Offers Sliders
+
+gsap.registerPlugin(ScrollTrigger);
+
+// Left Article
+gsap.from("#article__specialOffers__main", {
+  x: "-170%", 
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#section__specialOffers", 
+    start: "bottom bottom", 
+    toggleActions: "play none none none",
+  },
+});
+
+// Rigth Article
+gsap.from("#div__specialOffers", {
+  x: "170%",
+  duration: 1, 
+  scrollTrigger: {
+    trigger: "#section__specialOffers", 
+    start: "bottom bottom", 
+    end: "center center", 
+    toggleActions: "play none none none"
+  },
+});
 
   
