dda7a01bac2e4906e335c334b88c29db9ad1dd7b
PeterMonev <monevpeter@gmail.com>
2023-09-10 01:55:48 +0300

    Creating responsive

--- client/animation.js
+++ client/animation.js
@@ -1,7 +1,7 @@
 //Sticky Navigation Bar
 const lowerNav = document.querySelector("#lower__nav");
 
-// COMMENT THIS FUCNTION FOR PIXEL PERFECT! 
+// COMMENT THIS FUCNTION FOR PIXEL PERFECT!
 
 // function setSticky() {
 //     gsap.set(lowerNav, {
@@ -24,15 +24,15 @@ const lowerNav = document.querySelector("#lower__nav");
 //     onLeaveBack: unsetSticky
 // });
 
-$(document).ready(function() {
-  $('#navLower__ul__buttons .carousel-li button').on('click', function() {
-      const buttonColor = $(this).css('background-color');
+$(document).ready(function () {
+  $("#navLower__ul__buttons .carousel-li button").on("click", function () {
+    const buttonColor = $(this).css("background-color");
 
-      $('#section__searchBar').css('background-color', buttonColor);
+    $("#section__searchBar").css("background-color", buttonColor);
 
-      $('.pointer').remove();
+    $(".pointer").remove();
 
-      const pointerDiv = $(`
+    const pointerDiv = $(`
           <div class="pointer" style="display: none;">
               <svg xmlns="http://www.w3.org/2000/svg" width="17" height="11" viewBox="0 0 17 11" fill="none">
                   <path d="M7.81327 0.941857C8.23888 0.446402 9.00581 0.446401 9.43142 0.941856L16.3083 8.94738C16.9024 9.63902 16.411 10.709 15.4992 10.709H1.74545C0.833657 10.709 0.342243 9.63902 0.936378 8.94738L7.81327 0.941857Z" fill="${buttonColor}"/>
@@ -40,36 +40,41 @@ $(document).ready(function() {
           </div>
       `);
 
-      $(this).closest('.carousel-li').append(pointerDiv);
-      pointerDiv.fadeIn(500);
+    $(this).closest(".carousel-li").append(pointerDiv);
+    pointerDiv.fadeIn(500);
   });
 });
 
-
-
 // Lower Navigation Bar Carousel
 
-const elem = document.querySelector('.carousel-ul');
-const flkty = new Flickity( elem, {
-  cellAlign: 'left',
+const elem = document.querySelector(".carousel-ul");
+const flkty = new Flickity(elem, {
+  cellAlign: "left",
   contain: true,
-  pageDots: false, 
+  pageDots: false,
   prevNextButtons: true,
-  groupCells: true
+  groupCells: true,
 });
 
 // Toggle Button for responsive menu
 
-  $(".responsive__menu_btn").click(function() {
-    if ($("#nav__info").is(':hidden')) {
-      $("#nav__info").show().addClass("d-flex");
+$(".responsive__menu_btn").click(function () {
+  if ($("#nav__info").is(":hidden")) {
+    $("#nav__info").show().addClass("d-flex");
   } else {
-    $("#nav__info").hide().removeClass("d-flex")
+    $("#nav__info").hide().removeClass("d-flex");
   }
-      
-  });
+});
 
+// Toggle Button for responsive search buttons
 
+$(".search__button__responsive").click(function() {
+ if($("#div__search___buttons").is(":hidden")) {
+  $("#div__search___buttons").show().addClass("d-flex");
+ } else {
+   $("#div__search___buttons").hide().removeClass("d-flex")
+ }
+})
 
 // Carousel Banner Images
 $("#carouselExampleIndicators").swipe({
@@ -90,7 +95,6 @@ $("#div__specialOffers__offers").flickity({
   draggable: false,
   freeScroll: false,
   wrapAround: false,
-  
 });
 
 // Special Offers Sliders
@@ -146,18 +150,19 @@ gsap.from("#article__lastOffers__main", {
   },
 });
 
-// Rigth Articles 
+// Rigth Articles
 gsap.from("#div__lastOffers", {
   x: "-125%",
   duration: 1,
   scrollTrigger: {
-    trigger:  "#section__infoBlocks",
+    trigger: "#section__infoBlocks",
     start: "top bottom",
     toggleActions: "play none none none",
-  }
-})
+  },
+});
 
 // Info Blocks
+let windowWidth = window.innerWidth;
 
 // First Block
 
@@ -165,11 +170,11 @@ gsap.from("#article__infoBlocks__first", {
   x: "-290%",
   duration: 1,
   scrollTrigger: {
-    trigger:  "#section__smallInfoBlocks",
+    trigger: "#section__smallInfoBlocks",
     start: "top bottom",
     toggleActions: "play none none none",
-  }
-})
+  },
+});
 
 // Second Block
 
@@ -177,11 +182,11 @@ gsap.from("#article__infoBlocks__second", {
   x: "-290%",
   duration: 1,
   scrollTrigger: {
-    trigger:  "#section__smallInfoBlocks",
+    trigger: "#section__smallInfoBlocks",
     start: "top bottom",
     toggleActions: "play none none none",
-  }
-})
+  },
+});
 
 // Third Block
 
@@ -189,11 +194,11 @@ gsap.from("#article__infoBlocks__third", {
   x: "300%",
   duration: 1,
   scrollTrigger: {
-    trigger:  "#section__smallInfoBlocks",
+    trigger: "#section__smallInfoBlocks",
     start: "top bottom",
     toggleActions: "play none none none",
-  }
-})
+  },
+});
 
 // Fourth Block
 
@@ -201,11 +206,11 @@ gsap.from("#article__infoBlocks__fourth", {
   x: "300%",
   duration: 1,
   scrollTrigger: {
-    trigger:  "#section__smallInfoBlocks",
+    trigger: "#section__smallInfoBlocks",
     start: "top bottom",
     toggleActions: "play none none none",
-  }
-})
+  },
+});
 
 // Small Info Block
 
@@ -215,11 +220,11 @@ gsap.from("#article__smallInfoBlocks__first", {
   x: "-290%",
   duration: 1,
   scrollTrigger: {
-    trigger:  ".section__comments",
+    trigger: ".section__comments",
     start: "top bottom",
     toggleActions: "play none none none",
-  }
-})
+  },
+});
 
 // Second Block
 
@@ -227,11 +232,11 @@ gsap.from("#article__smallInfoBlocks__second", {
   x: "-290%",
   duration: 1,
   scrollTrigger: {
-    trigger:  ".section__comments",
+    trigger: ".section__comments",
     start: "top bottom",
     toggleActions: "play none none none",
-  }
-})
+  },
+});
 
 // Third Block
 
@@ -239,11 +244,11 @@ gsap.from("#article__smallInfoBlocks__third", {
   x: "300%",
   duration: 1,
   scrollTrigger: {
-    trigger:  ".section__comments",
+    trigger: ".section__comments",
     start: "top bottom",
     toggleActions: "play none none none",
-  }
-})
+  },
+});
 
 // Fourth Block
 
@@ -251,20 +256,19 @@ gsap.from("#article__smallInfoBlocks__fourth", {
   x: "300%",
   duration: 1,
   scrollTrigger: {
-    trigger:  ".section__comments",
+    trigger: ".section__comments",
     start: "top bottom",
     toggleActions: "play none none none",
-  }
-})
+  },
+});
 
 // Comments Carousel
 
-const $imagesCarousel = jQuery('.carouselOfImages').flickity({
-
+const $imagesCarousel = jQuery(".carouselOfImages").flickity({
   accessibility: false,
   autoPlay: false,
   pauseAutoPlayOnHover: false,
-  cellAlign: 'center',
+  cellAlign: "center",
   contain: false,
   draggable: true,
   friction: 0.2,
@@ -277,40 +281,56 @@ const $imagesCarousel = jQuery('.carouselOfImages').flickity({
   rightToLeft: false,
   setGallerySize: true,
   watchCSS: false,
-  wrapAround: true
-
+  wrapAround: true,
 });
 
 function resizeCells() {
-  var flkty = $imagesCarousel.data('flickity');
+  var flkty = $imagesCarousel.data("flickity");
   var $current = flkty.selectedIndex;
   var $length = flkty.cells.length;
   var $imageNumLimit = 5;
   if ($length < $imageNumLimit) {
-    $imagesCarousel.flickity('destroy');
+    $imagesCarousel.flickity("destroy");
   }
-  jQuery('.carouselOfImages .carouselComment').removeClass("nextToSelectedLeft");
-  jQuery('.carouselOfImages .carouselComment').removeClass("nextToSelectedRight");
-  jQuery('.carouselOfImages .carouselComment').removeClass("nextToSelectedLeft2");
-  jQuery('.carouselOfImages .carouselComment').removeClass("nextToSelectedRight2");
-  jQuery('.carouselOfImages .carouselComment').eq($current - 1).addClass("nextToSelectedLeft");
-  jQuery('.carouselOfImages .carouselComment').eq($current - 2).addClass("nextToSelectedLeft2");
+  jQuery(".carouselOfImages .carouselComment").removeClass(
+    "nextToSelectedLeft"
+  );
+  jQuery(".carouselOfImages .carouselComment").removeClass(
+    "nextToSelectedRight"
+  );
+  jQuery(".carouselOfImages .carouselComment").removeClass(
+    "nextToSelectedLeft2"
+  );
+  jQuery(".carouselOfImages .carouselComment").removeClass(
+    "nextToSelectedRight2"
+  );
+  jQuery(".carouselOfImages .carouselComment")
+    .eq($current - 1)
+    .addClass("nextToSelectedLeft");
+  jQuery(".carouselOfImages .carouselComment")
+    .eq($current - 2)
+    .addClass("nextToSelectedLeft2");
   var $endCell;
   if ($current + 1 == $length) {
     $endCell = "0";
   } else {
     $endCell = $current + 1;
   }
-  jQuery('.carouselOfImages .carouselComment').eq($endCell).addClass("nextToSelectedRight");
-  if($endCell + 1 < $imageNumLimit){
-    jQuery('.carouselOfImages .carouselComment').eq($endCell + 1).addClass("nextToSelectedRight2"); 
+  jQuery(".carouselOfImages .carouselComment")
+    .eq($endCell)
+    .addClass("nextToSelectedRight");
+  if ($endCell + 1 < $imageNumLimit) {
+    jQuery(".carouselOfImages .carouselComment")
+      .eq($endCell + 1)
+      .addClass("nextToSelectedRight2");
   } else {
-    jQuery('.carouselOfImages .carouselComment').eq(0).addClass("nextToSelectedRight2");
+    jQuery(".carouselOfImages .carouselComment")
+      .eq(0)
+      .addClass("nextToSelectedRight2");
   }
 }
 resizeCells();
 
-$imagesCarousel.on('scroll.flickity', function() {
+$imagesCarousel.on("scroll.flickity", function () {
   resizeCells();
 });
-
